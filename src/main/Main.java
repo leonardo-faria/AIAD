3f6fc02012894032f4f6a0dec96b84f32f9f9bd6
Vítor Teixeira <vitor_filipe_@hotmail.com>
2015-10-05 16:34:24 +0100

    asd

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,63 @@
 package main;
 
-import model.Model;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import sajas.core.Runtime;
+import sajas.sim.repast3.Repast3Launcher;
+import sajas.wrapper.ContainerController;
 import uchicago.src.sim.engine.SimInit;
 
-public class Main {
+public class Main extends Repast3Launcher {
 
 	private static final boolean BATCH_MODE = true;
+	private ContainerController mainContainer;
+	private ContainerController agentContainer;
+	public static final boolean SEPARATE_CONTAINERS = false;
+	public static final int NUM_AGENTS = 2;
+	
 
 	public static void main(String[] args) {
 		boolean runMode = !BATCH_MODE; // BATCH_MODE or !BATCH_MODE
-		// create a simulation
 		SimInit init = new SimInit();
-		// create a model
-		Model model = new Model();
-		// load model into simulation:
-		init.loadModel(model, null, runMode);
+		init.setNumRuns(10);   // works only in batch mode
+		init.loadModel(new Main(), null, BATCH_MODE);
+	}
+	
+	private void launchAgents() {
+		try {
+			
+		}
+		catch(Exception e) {
+			
+		}
+	}
+	
+	@Override
+	public String[] getInitParam() {
+		return new String[0];
+	}
+
+
+	@Override
+	public String getName() {
+		return "Transportes";
+	}
+
+
+	@Override
+	protected void launchJADE() {
+		Runtime rt = Runtime.instance();
+		Profile p1 = new ProfileImpl();
+		mainContainer = rt.createMainContainer(p1);
+		
+		if(SEPARATE_CONTAINERS) {
+			Profile p2 = new ProfileImpl();
+			agentContainer = rt.createAgentContainer(p2);
+		} else {
+			agentContainer = mainContainer;
+		}
+		
+		launchAgents();
 	}
 
 }
